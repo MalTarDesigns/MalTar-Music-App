cbb10423cacbec4a12472c6c65310168f48de49d
Jay Johnson <JayJohnson38@gmail.com>
2018-12-01 16:11:50 -0500

    added password confirmation (#29)

--- src/app/user/components/register/register.component.ts
+++ src/app/user/components/register/register.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as fromAuth from '../../../auth/store';
 import { UserService } from '../../../user/services/user-service/user.service';
+import { ErrorStateMatcher } from '@angular/material';
+import { getAuthError } from '../../../auth/store';
+
+// Ref: https://stackoverflow.com/questions/51605737/confirm-password-validation-in-angular-6
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -14,6 +26,7 @@ import { UserService } from '../../../user/services/user-service/user.service';
 export class RegisterComponent implements OnInit {
   registerFormGroup: FormGroup;
   errorMessage$: Observable<string>;
+  matcher = new MyErrorStateMatcher();
 
   constructor(
     private fb: FormBuilder,
@@ -22,13 +35,16 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    // TODO: Add some custom validation
     this.registerFormGroup = this.fb.group({
-      name: ['', [Validators.required]],
+      // name: ['', [Validators.required]],
       email: ['', [Validators.email, Validators.required]],
       password: ['', Validators.compose([Validators.minLength(6), Validators.required])],
       confirmPassword: ['', Validators.compose([Validators.minLength(6), Validators.required])]
-    });
-    // this.errorMessage$ = this.store.select(fromAuth.getAuthError);
+    }, { validator: this.checkPasswords });
+
+    this.errorMessage$ = this.store.select(getAuthError);
+    this.errorMessage$.subscribe(res => console.log(res))
   }
 
   onRegisterSubmit() {
@@ -38,6 +54,12 @@ export class RegisterComponent implements OnInit {
       email: user.email,
       password: user.password
     }));
+  }
+
+  checkPasswords(group: FormGroup) {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmPassword.value;
+    return pass === confirmPass ? null : { notSame: true }
   }
 
 }
